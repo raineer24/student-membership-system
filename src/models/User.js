2b17e40771d700380aac412186b2f433b1204535
raineer <raineerdelarita@gmail.com>
2025-05-07 12:29:12 +0800

    models user

--- src/models/User.js
+++ src/models/User.js
@@ -1,1 +1,13 @@
-const users = [];
+const users = [
+    {
+        id: 1,
+        email: 'seryra1@gmail.com',
+        password: '$2a$10$examplehashedpassword',
+        role: 'admin'
+    }
+];
+
+module.exports = {
+    findByEmail: (email) => users.find(user => user.email === email),
+    findById: (id) => users.find(user => user.id === id)
+}
