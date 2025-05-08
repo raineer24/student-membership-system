246c36b5dde3ab66d04b87dcefb2f2bbb501f1a7
raineer <raineerdelarita@gmail.com>
2025-05-08 13:41:35 +0800

    exports authenticate

--- src/utils/authMiddleware.js
+++ src/utils/authMiddleware.js
@@ -1,2 +1,15 @@
 const jwt = require('jsonwebtoken');
-const { JWT_SECRET} = process.env
+const { JWT_SECRET} = process.env;
+
+exports.authenticate = (req, res, next) => {
+    const token = req.header('Authorization')?.replace('Bearer ', '');
+    if (!token) return res.status(401).json({ error: 'Access denied'});
+
+    try {
+        req.user = jwt.verify(token, JWT_SECRET);
+        next();
+    } catch (error) {
+        res.status(401).json({error: 'Invalid token'}) ;
+        
+    }
+}
