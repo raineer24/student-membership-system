1b08c774864e06cee13b7dfc2a7a79f73ffdaf57
raineer <raineerdelarita@gmail.com>
2025-05-08 13:57:27 +0800

    app js parseBody

--- src/app.js
+++ src/app.js
@@ -1,1 +1,19 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const authRoutes = require('./routes/auth');
+const studentRoutes = require('./routes/students');
+//const paymentRoutes = require('./routes/payments')
+
+const app = express();
+
+// Middlewre
+app.use(cors());
+app.use(bodyParser.json());
+
+//Routes
+app.use('/api/auth', authRoutes);
+app.use('/api/students', studentRoutes);
+//app.use('/api/payments', pay);\
+
+
