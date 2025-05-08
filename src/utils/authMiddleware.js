436cb34e74dc32b14d0aa0b59ea64e1481ee273e
raineer <raineerdelarita@gmail.com>
2025-05-08 18:50:25 +0800

    authorize authmiddlware

--- src/utils/authMiddleware.js
+++ src/utils/authMiddleware.js
@@ -1,15 +1,20 @@
-const jwt = require('jsonwebtoken');
-const { JWT_SECRET} = process.env;
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = process.env;
 
 exports.authenticate = (req, res, next) => {
-    const token = req.header('Authorization')?.replace('Bearer ', '');
-    if (!token) return res.status(401).json({ error: 'Access denied'});
+  const token = req.header("Authorization")?.replace("Bearer ", "");
+  if (!token) return res.status(401).json({ error: "Access denied" });
 
-    try {
-        req.user = jwt.verify(token, JWT_SECRET);
-        next();
-    } catch (error) {
-        res.status(401).json({error: 'Invalid token'}) ;
-        
+  try {
+    req.user = jwt.verify(token, JWT_SECRET);
+    next();
+  } catch (error) {
+    res.status(401).json({ error: "Invalid token" });
+  }
+};
+
+exports.authorize = (roles = []) => {
+    return (req,res, next) => {
+
     }
 }
