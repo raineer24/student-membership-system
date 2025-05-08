78d8b0d909b77cb9a980d397d452d1e1665b6a0c
raineer <raineerdelarita@gmail.com>
2025-05-08 20:26:27 +0800

    authorize roles routes students

--- src/routes/students.js
+++ src/routes/students.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const studentController = require("../controllers/students");
-const { authenticate } = require("../utils/authMiddleware");
+const { authenticate, authorize } = require("../utils/authMiddleware");
+const { ROLES } = require('../config');
 
 const router = express.Router();
 router.use(authenticate);
 
 router.get('/', studentController.getAllStudents);
 router.get('/:id', studentController.getStudent);
-router.post('/', studentController.createStudent);
-router.put('/:id', studentController.updateStudent);
-router.delete('/:id', studentController.deleteStudent);
+router.post('/', authorize([ROLES.ADMIN]), studentController.createStudent);
+router.put('/:id', authorize([ROLES.ADMIN]), studentController.updateStudent);
+router.delete('/:id', authorize([ROLES.ADMIN]), studentController.deleteStudent);
 
 
 module.exports = router;
